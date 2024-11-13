3bbdd101d83b48d82402f232c20c8ab9a20da3ce
cless <lessc267@gmail.com>
2024-11-13 18:00:45 +0800

    name-update

--- src/App.tsx
+++ src/App.tsx
@@ -132,7 +132,7 @@ function App() {
             <Palette className="h-8 w-8" />
             <h1 className="text-xl font-semibold">
               疯狂的调色盘
-              <span className="ml-2 font-normal text-[hsl(0,0%,75%)] font-logo">· by chao</span>
+              <span className="ml-2 font-normal text-[hsl(0,0%,60%)] font-logo">· by charles</span>
             </h1>
           </div>
           <div className="flex gap-4">
