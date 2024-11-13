c16756804617b04c866160d0cef085b3a64c5995
cless <lessc267@gmail.com>
2024-11-13 14:40:41 +0800

    githubpage-dep

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Color_Card_List/',
   plugins: [react()],
   server: {
     port: 3000,
